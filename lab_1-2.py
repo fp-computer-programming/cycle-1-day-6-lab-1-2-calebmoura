fb9f75035e8fcba602bc914003f54900bceea818
calebmoura <144838461+calebmoura@users.noreply.github.com>
2023-09-25 20:00:56 -0400

    Add files via upload

---
+++ lab_1-2.py
@@ -0,0 +1,13 @@
+# Author: CAM 09/25/23
+
+def name (variable):
+    def caleb (variable):
+        name = caleb
+    print(name)
+
+def favorite_hobby (variable):
+    def playing_soccer (variable):
+        favorite_hobby = playing_soccer
+    print (favorite_hobby)
+
+# nothing happens in output, not sure why, no "problems" detected
